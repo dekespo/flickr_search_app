bf6075d2527ab4751da9cd9badbbd57e45429102
Dekespo <dekespo@gmail.com>
2019-12-11 21:55:06 +0100

    First listView in a fragment

--- app/src/main/java/com/dekespo/flickr_search_app/ui/home/HomeFragment.java
+++ app/src/main/java/com/dekespo/flickr_search_app/ui/home/HomeFragment.java
@@ -1,23 +1,38 @@
 package com.dekespo.flickr_search_app.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
+import com.dekespo.flickr_search_app.R;
+
+import java.util.ArrayList;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-
-import com.dekespo.flickr_search_app.R;
 
 public class HomeFragment extends Fragment
 {
 
     private HomeViewModel homeViewModel;
+    private Context mContext;
+
+    @Override
+    public void onAttach(@NonNull Context context)
+    {
+        super.onAttach(context);
+        mContext = context;
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
@@ -34,6 +49,28 @@ public class HomeFragment extends Fragment
                 textView.setText(s);
             }
         });
+
+
+        final ListView list = root.findViewById(R.id.list);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("JAVA");
+        arrayList.add("ANDROID");
+        arrayList.add("C Language");
+        arrayList.add("CPP Language");
+        arrayList.add("Go Language");
+        arrayList.add("AVN SYSTEMS");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(mContext, android.R.layout.simple_list_item_1, arrayList);
+        list.setAdapter(arrayAdapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                String clickedItem = (String) list.getItemAtPosition(position);
+                Toast.makeText(mContext, clickedItem, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return root;
     }
 }
