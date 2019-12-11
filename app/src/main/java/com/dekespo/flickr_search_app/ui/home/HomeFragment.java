a60d1f689c202c53cf282c0fe6b126ed7b2a5008
Dekespo <dekespo@gmail.com>
2019-12-11 21:59:32 +0100

    Removed the textbox from home fragment

--- app/src/main/java/com/dekespo/flickr_search_app/ui/home/HomeFragment.java
+++ app/src/main/java/com/dekespo/flickr_search_app/ui/home/HomeFragment.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dekespo.flickr_search_app.R;
@@ -16,9 +15,7 @@ import com.dekespo.flickr_search_app.R;
 import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 public class HomeFragment extends Fragment
@@ -40,16 +37,6 @@ public class HomeFragment extends Fragment
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>()
-        {
-            @Override
-            public void onChanged(@Nullable String s)
-            {
-                textView.setText(s);
-            }
-        });
-
 
         final ListView list = root.findViewById(R.id.list);
         ArrayList<String> arrayList = new ArrayList<>();
