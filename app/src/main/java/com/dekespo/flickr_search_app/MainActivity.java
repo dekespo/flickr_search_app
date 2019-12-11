1089c5ecb984539d3ebf1bf785911284beee5b77
Dekespo <dekespo@gmail.com>
2019-12-11 21:37:54 +0100

    More clean up with lint

--- app/src/main/java/com/dekespo/flickr_search_app/MainActivity.java
+++ app/src/main/java/com/dekespo/flickr_search_app/MainActivity.java
@@ -29,9 +29,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
