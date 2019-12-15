8cce84f17458957848471418ecd1031f2dfc9a8e
Dekespo <dekespo@gmail.com>
2019-12-15 22:15:58 +0100

    Added permission check

--- app/src/main/java/com/dekespo/flickr_search_app/MainActivity.java
+++ app/src/main/java/com/dekespo/flickr_search_app/MainActivity.java
@@ -1,6 +1,11 @@
 package com.dekespo.flickr_search_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.anthonycr.grant.PermissionsManager;
+import com.anthonycr.grant.PermissionsResultAction;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +18,24 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PermissionsManager.getInstance().requestAllManifestPermissionsIfNecessary(this, new PermissionsResultAction()
+        {
+            @Override
+            public void onGranted()
+            {
+                Log.d(TAG, "permission is allowed!");
+            }
+
+            @Override
+            public void onDenied(String permission)
+            {
+                Log.e(TAG, "Permission denied: " + permission);
+                Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                homeIntent.addCategory(Intent.CATEGORY_HOME);
+                homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
+            }
+        });
     }
 }
